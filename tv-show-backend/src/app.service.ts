38854fdb3548334969ac7d52839d15f928eacc5e
hamza <mahjoubhamza036@gmail.com>
2022-12-24 21:22:49 +0100

    test eslint

--- tv-show-backend/src/app.service.ts
+++ tv-show-backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  // default service   
+  // default service
   getHello(): string {
     return 'Hello World!';
   }
