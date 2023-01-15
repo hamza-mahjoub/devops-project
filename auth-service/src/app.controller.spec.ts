b569060f63c911d069336f81879f23ee67666f2d
hamza <mahjoubhamza036@gmail.com>
2023-01-16 00:33:07 +0100

    workflow

--- auth-service/src/app.controller.spec.ts
+++ auth-service/src/app.controller.spec.ts
@@ -1,16 +1,32 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import { makeCounterProvider } from '@willsoto/nestjs-prometheus';
+import { makeCounterProvider, PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { RequestInterceptor } from './interceptors/request.interceptor';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { UsersModule } from './users/users.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule, UsersModule,
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        PrometheusModule.register(),
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) => ({
+            uri: configService.get('CONNECTION_STRING'),
+          }),
+          inject: [ConfigService],
+        })],
       controllers: [AppController],
       providers: [AppService,
         makeCounterProvider({
