de1cbbaec8700bf2074a1b723a3ee25bbe0cbca8
hamza <mahjoubhamza036@gmail.com>
2023-01-16 00:22:07 +0100

    fixed auth service

--- auth-service/src/app.controller.spec.ts
+++ auth-service/src/app.controller.spec.ts
@@ -1,6 +1,10 @@
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
+import { makeCounterProvider } from '@willsoto/nestjs-prometheus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RequestInterceptor } from './interceptors/request.interceptor';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +12,20 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService,
+        makeCounterProvider({
+          name: "requests_metric",
+          help: "Counts number of processed requests",
+          labelNames: ["requests_count", "http_route"]
+        }),
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: RequestInterceptor
+        },
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: ResponseInterceptor
+        }],
     }).compile();
 
     appController = app.get<AppController>(AppController);
