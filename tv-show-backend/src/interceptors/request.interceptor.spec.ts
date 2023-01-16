b824335f734fe218de3949e4c1ff32d32e2a2208
hamza <mahjoubhamza036@gmail.com>
2023-01-16 17:22:44 +0100

    manifests updates

--- tv-show-backend/src/interceptors/request.interceptor.spec.ts
+++ tv-show-backend/src/interceptors/request.interceptor.spec.ts
@@ -3,7 +3,7 @@ import { RequestInterceptor } from './request.interceptor';
 
 describe('RequestInterceptor', () => {
 
-  it('should be defined', () => {
+  it('should be defined', () => { 
     const counter = new Counter<string>({ name: "counter", help: "a counter" });
 
     expect(new RequestInterceptor(counter)).toBeDefined();
